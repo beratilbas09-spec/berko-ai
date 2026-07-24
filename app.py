9c41bee7a723a9b6d832a1bd070cf207e25d56f3
beratilbas09-spec <beratilbas09@gmail.com>
2026-07-24 14:31:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import base64
 
 # Sayfa Ayarları (Yüklediğin jpeg dosyasını buraya ekledik kanka)
 st.set_page_config(
-    page_title="B",
+    page_title="Berko Ai",
     page_icon="bane.jpg",
     layout="centered"
 )
